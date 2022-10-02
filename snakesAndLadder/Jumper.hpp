7da3f088f5d9270f86fe5f2353a0ddfb80fad31e
Rajashekar Reddy Kommula <32640055+Rajashekarredde@users.noreply.github.com>
2022-10-02 16:14:47 -0700

    Create Jumper.hpp

---
+++ snakesAndLadder/Jumper.hpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Jumper
+{
+    public:
+    
+    int startPoint;
+    int endPoint;
+    
+     Jumper( int _start, int _end )
+     {
+         this->startPoint = _start;
+         this->endPoint = _end;
+     }
+};
