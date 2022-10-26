8c941cbf000813f41c7bea27aaa6866aa525c2b8
Rajashekar Reddy Kommula <32640055+Rajashekarredde@users.noreply.github.com>
2022-10-26 09:00:12 -0700

    Update Player.hpp

--- 2048/Player.hpp
+++ 2048/Player.hpp
@@ -4,16 +4,28 @@ class Player
 {
     string name;
     int id;
+    long long score;
     public:
         Player() {}
         Player( string _name, int _id )
         {
             this->name = _name;
             this->id = _id;
+            this->score = 0;
         }
         
-        string getPlayeName()
+        string getPlayerName()
         {
             return this->name;
+        }
+        
+        void addScore( int val )
+        {
+            score += val;
+        }
+        
+        long long getScore()
+        {
+            return score;
         }
 };
